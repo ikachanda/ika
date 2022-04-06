8e99bab903f56602dcf3f0c8ac4a95fde1857c29
ikachanda <103010523+ikachanda@users.noreply.github.com>
2022-04-06 09:51:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,11 +5,11 @@ import traceback
 bot = commands.Bot(command_prefix='')
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+#@bot.event
+#async def on_command_error(ctx, error):
+#    orig_error = getattr(error, "original", error)
+#    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+#    await ctx.send(error_msg)
 
 
 @bot.command()
