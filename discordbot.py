db99b701f47f3d48563039a6863e7e5fa2efe0ff
ikachanda <103010523+ikachanda@users.noreply.github.com>
2022-04-06 17:04:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,6 +23,22 @@ async def ap(ctx, *args):
         api.append(f'https://uniteapi.dev/p/{val}')
     await ctx.send("\n".join(api))
 
+@bot.command()
+async def te(ctx):
+    embed = discord.Embed( # Embedを定義する
+                          title = "スプラトゥーン",# タイトル
+                          color = 0x00ff00, # フレーム色指定(今回は緑)
+                          description = "とりあえずのテスト", # Embedの説明文 必要に応じて
+                          #url="https://example.com" # これを設定すると、タイトルが指定URLへのリンクになる
+                          )
+    embed.set_author(name = message.author.name, 
+                     icon_url=message.author.avatar_url # Botのアイコンを設定してみる
+                     )
+    embed.set_image(url="https://image.example.com/main.png") # 大きな画像タイルを設定できる
+    embed.set_footer(text=f"Requested {message.author.name}")
+
+    await ctx.send('embed = embed')
+
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
