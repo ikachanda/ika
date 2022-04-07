c0ead9564796422f81410c60943781b2ab69e77c
ikachanda <103010523+ikachanda@users.noreply.github.com>
2022-04-07 15:39:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,7 +29,7 @@ async def ap(ctx, *args):
 async def te(ctx):
     embed=discord.Embed(color=0xf7e37e, timestamp=datetime.datetime.utcnow())
     embed.add_field(name="スプラシューター", value="いいよ", inline=False)
-    embed.set_footer(text='なんでだろう')
+    embed.set_footer(text='Requested')
     
     await ctx.send(embed = embed)
 
