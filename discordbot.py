0bce92e8010d36259cde6d8e9c4184fd610ce22d
ikachanda <103010523+ikachanda@users.noreply.github.com>
2022-04-08 21:50:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,38 @@
 from discord.ext import commands
- from os import getenv
- import traceback
+from os import getenv
+import traceback
+import discord
+from datetime import datetime
 
- bot = commands.Bot(command_prefix='/')
-
-
- @bot.event
- async def on_command_error(ctx, error):
-     orig_error = getattr(error, "original", error)
-     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-     await ctx.send(error_msg)
+bot = commands.Bot(command_prefix='')
 
 
- @bot.command()
- async def ping(ctx):
-     await ctx.send('pong')
+#@bot.event
+#async def on_command_error(ctx, error):
+#    orig_error = getattr(error, "original", error)
+#    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+#    await ctx.send(error_msg)
 
 
- token = getenv('DISCORD_BOT_TOKEN')
- bot.run(token)
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def ap(ctx, *args):
+    api = []
+    for val in args:
+        api.append(f'https://uniteapi.dev/p/{val}')
+    await ctx.send("\n".join(api))
+
+@bot.command()
+async def te(ctx):
+    embed=discord.Embed(color=0xf7e37e, timestamp=datetime.utcnow())
+    embed.add_field(name="スプラシューター", value="いいよ", inline=False)
+    embed.set_footer(text = f"Requested by {ctx.author.name}")
+    
+    await ctx.send(embed = embed)
+
+
+token = getenv('DISCORD_BOT_TOKEN')
+bot.run(token)
