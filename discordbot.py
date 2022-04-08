5614d1cb3e44c93500a497de60491476e245b1e5
ikachanda <103010523+ikachanda@users.noreply.github.com>
2022-04-08 21:41:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,38 +1,21 @@
 from discord.ext import commands
-from os import getenv
-import traceback
-import discord
-from datetime import datetime
+ from os import getenv
+ import traceback
 
-bot = commands.Bot(command_prefix='')
+ bot = commands.Bot(command_prefix='/')
 
 
-#@bot.event
-#async def on_command_error(ctx, error):
-#    orig_error = getattr(error, "original", error)
-#    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-#    await ctx.send(error_msg)
+ @bot.event
+ async def on_command_error(ctx, error):
+     orig_error = getattr(error, "original", error)
+     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+     await ctx.send(error_msg)
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def ap(ctx, *args):
-    api = []
-    for val in args:
-        api.append(f'https://uniteapi.dev/p/{val}')
-    await ctx.send("\n".join(api))
-
-@bot.command()
-async def te(ctx):
-    embed=discord.Embed(color=0xf7e37e, timestamp=datetime.utcnow())
-    embed.add_field(name="スプラシューター", value="いいよ")
-    embed.set_footer(text = f"Requested by {ctx.author.name}")
-    
-    await ctx.send(embed = embed)
+ @bot.command()
+ async def ping(ctx):
+     await ctx.send('pong')
 
 
-token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+ token = getenv('DISCORD_BOT_TOKEN')
+ bot.run(token)
