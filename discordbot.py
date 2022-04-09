0d82350f55c2ff30aa23b386cdd7d41724357288
ikachanda <103010523+ikachanda@users.noreply.github.com>
2022-04-09 21:08:49 +0900

    delete

--- discordbot.py
+++ discordbot.py
@@ -16,7 +16,7 @@ bot = commands.Bot(command_prefix='')
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.message.delete(ctx)
 
 @bot.command()
 async def ap(ctx, *args):
